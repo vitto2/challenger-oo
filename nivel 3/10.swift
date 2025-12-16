78d22c7cd70582ddaf66a35407f9b6747a02d8fc
Vitor Silva <vitor2071@gmail.com>
2025-12-16 05:21:19 -0300

    feat: implementa desafio 10 nivel 3

--- nivel 3/10.swift
+++ nivel 3/10.swift
@@ -2,21 +2,48 @@
 import Foundation
 
 class Booking {
-  private let code: String
-  private let dataCheckin: String
-  private let dataCheckout: String
+    private let code: String
+    private let dataCheckin: String
+    private let dataCheckout: String
+  
+     struct DateFormat {
+      let day: Int 
+      let month: Int
+      let year: Int
+    }
 
+    init(dataCheckin: String, dataCheckout: String) {
+      let randomCode = Int.random(in: 1000...2000)
+      code = String(randomCode)
+      self.dataCheckin = dataCheckin
+      self.dataCheckout = dataCheckout
+    }
 
-  init(dataCheckin: String, dataCheckout: String) {
-    let randomCode = Int.random(in: 1000...2000)
-    code = String(randomCode)
-    self.dataCheckin = dataCheckin
-    self.dataCheckout = dataCheckout
-  }
+    private func formatedData(date: String) -> DateFormat {
+        let dateNormalize = date.split(separator: "-")
+        .map {Int($0) ?? 0}
+        
+      return DateFormat(day: dateNormalize[2], month: dateNormalize[1], year: dateNormalize[0])
+      }
 
+    func showInfos() {
+      print("\(code) | \(dataCheckin) | \(dataCheckout)")
+    }
 
-  func showInfos() {
-    print("\(code) | \(dataCheckin) | \(dataCheckout)")
+   func calcularDuracao() {
+    let dataCheckInStructured = formatedData(date: dataCheckin)
+    let dataCheckOutStructured = formatedData(date: dataCheckout)
+
+    let validateDay = dataCheckOutStructured.day > dataCheckInStructured.day
+    let validateMonth = dataCheckOutStructured.month >= dataCheckInStructured.month
+    let validateYear = dataCheckOutStructured.year >= dataCheckInStructured.year
+    
+    if validateDay && validateMonth && validateYear { 
+      let dias = dataCheckOutStructured.day - dataCheckInStructured.day
+      print("Duração: \(dias) dia(s)")
+    } else {
+      print("Período inválido. Tente novamente.")
+    }
   }
 }
 
@@ -24,4 +51,4 @@ class Booking {
 let bkn1 = Booking(dataCheckin: "2025-12-15", dataCheckout: "2025-12-20")
 
 
-bkn1.showInfos()
+bkn1.calcularDuracao()
